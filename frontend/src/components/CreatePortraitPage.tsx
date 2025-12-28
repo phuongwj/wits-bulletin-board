c0f87058ffee6c60aabd4923747e70de008cd1b2
phuongwj <phamjulia9@gmail.com>
2025-12-28 17:13:24 -0400

    checkpoint for now

--- frontend/src/components/CreatePortraitPage.tsx
+++ frontend/src/components/CreatePortraitPage.tsx
@@ -1,5 +1,7 @@
 import { ReactSketchCanvas, type ReactSketchCanvasRef } from "react-sketch-canvas";
 import { useRef, useState } from "react";
+import '../styles/CreatePortraitPage.scss';
+
 
 export default function CreatePortraitPage() {
   const canvasRef = useRef<ReactSketchCanvasRef>(null);
@@ -27,10 +29,6 @@ export default function CreatePortraitPage() {
     canvasRef.current?.clearCanvas();
   }
 
-  const handleResetClick = () => {
-    canvasRef.current?.resetCanvas();
-  }
-
   return (
     <>
       <div>
@@ -43,7 +41,34 @@ export default function CreatePortraitPage() {
           >
             Pen
           </button>
+          <button
+            type="button"
+            disabled={eraseMode}
+            onClick={handleEraserClick}
+          >
+            Eraser
+          </button>
+          <button
+            type="button"
+            onClick={handleUndoClick}
+          >
+            Undo
+          </button>
+          <button
+            type="button"
+            onClick={handleRedoClick}
+          >
+            Redo
+          </button>
+          <button
+            type="button"
+            onClick={handleClearClick}
+          >
+            Clear
+          </button>
         </div>
+        <h1 id="hello">Canvas</h1>
+        <ReactSketchCanvas ref={canvasRef} canvasColor="transparent" width="1000px" height="400px"/>
       </div>
     </>
   )
